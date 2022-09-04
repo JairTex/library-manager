b712ffeaab4c6ed00d8a4eb54f05c1b44dc2512c
JairTex <steixeirajair@gmail.com>
2022-09-03 21:07:17 -0300

    Criação dos models e adição ao admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from core.models import Autor, Categoria, Leitor, Livro, Emprestimo
 
-# Register your models here.
+#Para a tabela do Model aparecer no /admin
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'criado', 'modificado', 'ativo')
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'criado', 'modificado', 'ativo')
+
+@admin.register(Leitor)
+class LeitorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'telefone', 'email', 'status', 'criado', 'modificado', 'ativo')
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nome', 'categoria', 'autor', 'status', 'criado', 'modificado', 'ativo')
+
+@admin.register(Emprestimo)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('leitor', 'livro', 'status', 'criado', 'modificado', 'ativo')
--- core/models.py
+++ core/models.py
@@ -1,3 +1,91 @@
 from django.db import models
+from django.db.models import signals
+from django.template.defaultfilters import slugify
+from stdimage.models import StdImageField
 
-# Create your models here.
+#Todas as classes vão herdar a classe base
+class Base(models.Model):
+    criado = models.DateField('Data de Criação', auto_now_add=True)
+    modificado = models.DateField('Data de Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default = True)
+
+    class Meta:
+        abstract = True
+
+
+class Categoria(Base):
+    nome = models.CharField('Nome', max_length = 30)
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
+    def __str__(self):
+        return self.nome 
+
+
+class Autor(Base):
+    nome = models.CharField('Nome', max_length = 100)
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+
+    def __str__(self):
+        return self.nome 
+
+class Livro(Base):
+    STATUS_CHOICE = (
+        (True, 'Disponível'),
+        (False, 'Indisponível')
+    )
+
+    codigo = models.CharField('Codigo', max_length=15, unique=True)
+    nome = models.CharField('Nome', max_length=100)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, default='Desconhecido')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, default='Geral')
+    status = models.CharField('Status', default='Disponível', max_length=12, choices=STATUS_CHOICE)
+
+    class Meta:
+        verbose_name = 'Livro'
+        verbose_name_plural = 'Livros'
+
+    def __str__(self):
+        return self.nome
+
+
+class Leitor(Base):
+    STATUS_CHOICE = (
+        (True, 'Desbloqueado'),
+        (False, 'Bloqueado')
+    )
+
+    nome = models.CharField('Nome', max_length=50)
+    telefone = models.CharField('Telefone', max_length=13)
+    email = models.EmailField('Email', max_length=50)
+    status = models.CharField('Status', default='Desbloqueado', max_length=12, choices=STATUS_CHOICE)
+
+    class Meta:
+        verbose_name = 'Leitor'
+        verbose_name_plural = 'Leitores'
+
+    def __str__(self):
+        return self.nome
+
+
+class Emprestimo(Base):
+    STATUS_CHOICE = (
+        (True, 'Em andamento'),
+        (False, 'Finalizado')
+    )
+
+    leitor = models.ForeignKey(Leitor, on_delete=models.CASCADE)
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    status = models.CharField('Status', default='Em andamento', max_length=12, choices=STATUS_CHOICE)
+
+    class Meta:
+        verbose_name = 'Emprestimo'
+        verbose_name_plural = 'Emprestimos'
+
+    def __str__(self):
+        return f'{self.leitor} | {self.livro}'
